ed126ea33760f115914fd43252543cbb6536b534
Guido <guido.andino@outlook.com>
2026-09-21 12:16:55 -0300

    Funciones
    Agregando operaciones en funciones

---
+++ modulo/mi_matematica.py
@@ -0,0 +1,35 @@
+
+def es_par(numero):
+    """Retorna True si el numero es par, False si es impar."""
+    if numero % 2 == 0:
+        print("true")
+        return True          
+    else:
+        print("False")
+        return False  
+
+
+def factorial(n):   
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+    
+
+def promedio_de_listas(lista_numeros):
+    if lista_numeros:
+        lista = sum(lista_numeros)/len(lista_numeros)
+    else:
+        print("Esto no es una lista")
+    return lista
+    
+
+def area_circulo(entrada):
+    if str(entrada).isdigit():
+        radio = int(entrada)
+        PI = 3.1416
+        return PI * (radio ** 2)
+    else: 
+        print("Esto no es un numero positivo.")     
+        
+    
+
